30a66d011854c8cf9f0ece85d2275816855fdd93
SpicyLollipop <143978518+SpicyLollipop@users.noreply.github.com>
2023-09-04 14:48:54 +0800

    Update User.java

--- User.java
+++ User.java
@@ -5,7 +5,7 @@ public class User
 	private String username;
 	private String password;
 	
-	public User(String username, String password)
+	public User(String username, String password)sadsas
 	{
 		this.username = username;
 		this.password = password;
